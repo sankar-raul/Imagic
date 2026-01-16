8a2b298b8ad78a721a79754188145de0646853b8
sankar-raul <raulsankar99@gmail.com>
2026-01-16 12:59:39 +0530

    feat: refactor TypeScript configuration, restore upload route, and clean up code structure

--- server/src/models/news/news.schema.ts
+++ server/src/models/news/news.schema.ts
@@ -1,12 +1,12 @@
 import { Schema } from "mongoose";
-import { Inews } from "../Types/news.types";
+import { Inews } from "../../Types/news.types";
 
 const newsSchema = new Schema<Inews>({
-    title: { type: String, required: true },
-    slug: { type: String, required: true, unique: true },
-    thumbnail: { type: String, required: true },
-    content: { type: String, required: true },
-    published_date: { type: Date, required: true },
+  title: { type: String, required: true },
+  slug: { type: String, required: true, unique: true },
+  thumbnail: { type: String, required: true },
+  content: { type: String, required: true },
+  published_date: { type: Date, required: true },
 });
 
 export default newsSchema;
