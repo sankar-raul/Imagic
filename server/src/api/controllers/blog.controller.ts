ab727c6d8e7dc0c4b01df8fcd61fd315b49bcd7d
sankar-raul <raulsankar99@gmail.com>
2026-01-19 18:26:38 +0530

    feat: enhance getBlogDetails to include previous and next blog slugs in response

--- server/src/api/controllers/blog.controller.ts
+++ server/src/api/controllers/blog.controller.ts
@@ -18,7 +18,26 @@ export const getBlogDetails = async (req: Request, res: Response) => {
     if (!blogDetails) {
       return res.status(404).json({ message: "Blog not found" });
     }
-    res.status(200).json({ data: blogDetails });
+
+    // Find previous and next blogs based on creation date or _id
+    const [prevBlog, nextBlog] = await Promise.all([
+      blog
+        .findOne({ _id: { $lt: blogDetails._id } })
+        .sort({ _id: -1 })
+        .select("slug")
+        .limit(1),
+      blog
+        .findOne({ _id: { $gt: blogDetails._id } })
+        .sort({ _id: 1 })
+        .select("slug")
+        .limit(1),
+    ]);
+
+    res.status(200).json({
+      data: blogDetails,
+      prevSlug: prevBlog?.slug || null,
+      nextSlug: nextBlog?.slug || null,
+    });
   } catch (error) {
     res.status(500).json({ message: (error as Error).message });
   }
