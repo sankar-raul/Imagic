a809203e3386d5c7358dd76bebcc11489a06daa3
sankar-raul <raulsankar99@gmail.com>
2026-01-19 18:32:15 +0530

    feat: add getBlogBySlug API method and integrate it into the API index

--- client/src/utils/api/blog/blog.ts
+++ client/src/utils/api/blog/blog.ts
@@ -1,48 +1,57 @@
-import {get, post, deleteRequest, put} from "../apiMethod";
+import { get, post, deleteRequest, put } from "../apiMethod";
 
-const INITIAL_ROUTE = '/blogs';
+const INITIAL_ROUTE = "/blogs";
 
 export const getAllBlogs = async () => {
-    try {
-        const response = await get(`${INITIAL_ROUTE}`);
-        return response;
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const response = await get(`${INITIAL_ROUTE}`);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const postBlog = async (blogData: any) => {
-    try {
-        const response = await post(`${INITIAL_ROUTE}`, blogData);
-        return response;
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const response = await post(`${INITIAL_ROUTE}`, blogData);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const getBlogById = async (blogId: string) => {
-    try {
-        const response = await get(`${INITIAL_ROUTE}/id/${blogId}`);
-        return response;
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const response = await get(`${INITIAL_ROUTE}/id/${blogId}`);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getBlogBySlug = async (slug: string) => {
+  try {
+    const response = await get(`${INITIAL_ROUTE}/${slug}`);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const updateBlog = async (blogId: string, blogData: any) => {
-    try {
-        const response = await put(`${INITIAL_ROUTE}/${blogId}`, blogData);
-        return response;
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const response = await put(`${INITIAL_ROUTE}/${blogId}`, blogData);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const deleteBlog = async (blogId: string) => {
-    try {
-        const response = await deleteRequest(`${INITIAL_ROUTE}/${blogId}`);
-        return response;
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const response = await deleteRequest(`${INITIAL_ROUTE}/${blogId}`);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
